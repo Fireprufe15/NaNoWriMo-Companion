5160727546a53797767b72f826025ea120246a3e
Fireprufe15 <mgandr@gmail.com>
2015-10-21 12:16:51 +0200

    Changed form design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtWordCount = new System.Windows.Forms.TextBox();
             this.btnSetWordCount = new System.Windows.Forms.Button();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.btnCheckCount = new System.Windows.Forms.Button();
+            this.btnWordCountHistory = new System.Windows.Forms.Button();
+            this.btnCheckOtherWordcount = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // txtWordCount
-            // 
-            this.txtWordCount.Location = new System.Drawing.Point(12, 91);
-            this.txtWordCount.Name = "txtWordCount";
-            this.txtWordCount.Size = new System.Drawing.Size(100, 20);
-            this.txtWordCount.TabIndex = 0;
             // 
             // btnSetWordCount
             // 
-            this.btnSetWordCount.Location = new System.Drawing.Point(118, 88);
+            this.btnSetWordCount.Location = new System.Drawing.Point(12, 12);
             this.btnSetWordCount.Name = "btnSetWordCount";
             this.btnSetWordCount.Size = new System.Drawing.Size(134, 23);
             this.btnSetWordCount.TabIndex = 1;
@@ -52,7 +47,7 @@
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(711, 13);
+            this.btnLogout.Location = new System.Drawing.Point(12, 151);
             this.btnLogout.Name = "btnLogout";
             this.btnLogout.Size = new System.Drawing.Size(75, 23);
             this.btnLogout.TabIndex = 2;
@@ -60,26 +55,55 @@
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
+            // btnCheckCount
+            // 
+            this.btnCheckCount.Location = new System.Drawing.Point(12, 41);
+            this.btnCheckCount.Name = "btnCheckCount";
+            this.btnCheckCount.Size = new System.Drawing.Size(134, 23);
+            this.btnCheckCount.TabIndex = 3;
+            this.btnCheckCount.Text = "Current Wordcount";
+            this.btnCheckCount.UseVisualStyleBackColor = true;
+            // 
+            // btnWordCountHistory
+            // 
+            this.btnWordCountHistory.Location = new System.Drawing.Point(152, 12);
+            this.btnWordCountHistory.Name = "btnWordCountHistory";
+            this.btnWordCountHistory.Size = new System.Drawing.Size(134, 23);
+            this.btnWordCountHistory.TabIndex = 4;
+            this.btnWordCountHistory.Text = "Word Count History";
+            this.btnWordCountHistory.UseVisualStyleBackColor = true;
+            // 
+            // btnCheckOtherWordcount
+            // 
+            this.btnCheckOtherWordcount.Location = new System.Drawing.Point(152, 41);
+            this.btnCheckOtherWordcount.Name = "btnCheckOtherWordcount";
+            this.btnCheckOtherWordcount.Size = new System.Drawing.Size(134, 23);
+            this.btnCheckOtherWordcount.TabIndex = 5;
+            this.btnCheckOtherWordcount.Text = "Other User Wordcount";
+            this.btnCheckOtherWordcount.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(798, 505);
+            this.ClientSize = new System.Drawing.Size(298, 186);
+            this.Controls.Add(this.btnCheckOtherWordcount);
+            this.Controls.Add(this.btnWordCountHistory);
+            this.Controls.Add(this.btnCheckCount);
             this.Controls.Add(this.btnLogout);
             this.Controls.Add(this.btnSetWordCount);
-            this.Controls.Add(this.txtWordCount);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtWordCount;
         private System.Windows.Forms.Button btnSetWordCount;
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnCheckCount;
+        private System.Windows.Forms.Button btnWordCountHistory;
+        private System.Windows.Forms.Button btnCheckOtherWordcount;
     }
 }
 
