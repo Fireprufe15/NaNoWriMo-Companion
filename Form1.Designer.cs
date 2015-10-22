31c56105aff216b2acb45210b2bf41e75b87de5f
MG Potgieter <mgandr@gmail.com>
2015-10-22 12:03:11 +0200

    Added more UI elements and the code for some of the statistics.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,16 @@
             this.btnCheckOtherWordcount = new System.Windows.Forms.Button();
             this.lblWC = new System.Windows.Forms.Label();
             this.lblWCNum = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblWordsRemaining = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWPD = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRemainingDays = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSetWordCount
@@ -49,7 +59,7 @@
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(12, 182);
+            this.btnLogout.Location = new System.Drawing.Point(111, 70);
             this.btnLogout.Name = "btnLogout";
             this.btnLogout.Size = new System.Drawing.Size(75, 23);
             this.btnLogout.TabIndex = 2;
@@ -89,7 +99,7 @@
             // 
             this.lblWC.AutoSize = true;
             this.lblWC.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWC.Location = new System.Drawing.Point(14, 67);
+            this.lblWC.Location = new System.Drawing.Point(14, 107);
             this.lblWC.Name = "lblWC";
             this.lblWC.Size = new System.Drawing.Size(272, 26);
             this.lblWC.TabIndex = 6;
@@ -99,18 +109,113 @@
             // 
             this.lblWCNum.AutoSize = true;
             this.lblWCNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWCNum.Location = new System.Drawing.Point(12, 93);
+            this.lblWCNum.Location = new System.Drawing.Point(12, 133);
             this.lblWCNum.Name = "lblWCNum";
             this.lblWCNum.Size = new System.Drawing.Size(70, 76);
             this.lblWCNum.TabIndex = 7;
             this.lblWCNum.Text = "0";
             this.lblWCNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 212);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(274, 214);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stats";
+            // 
+            // lblWordsRemaining
+            // 
+            this.lblWordsRemaining.AutoSize = true;
+            this.lblWordsRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWordsRemaining.Location = new System.Drawing.Point(134, 1);
+            this.lblWordsRemaining.Name = "lblWordsRemaining";
+            this.lblWordsRemaining.Size = new System.Drawing.Size(23, 25);
+            this.lblWordsRemaining.TabIndex = 1;
+            this.lblWordsRemaining.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Words Left:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.lblRemainingDays, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblWordsRemaining, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblWPD, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 19);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.67556F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.6789F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.6789F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.60886F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.6789F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.6789F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(262, 185);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "WPD To Finish:";
+            // 
+            // lblWPD
+            // 
+            this.lblWPD.AutoSize = true;
+            this.lblWPD.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWPD.Location = new System.Drawing.Point(134, 61);
+            this.lblWPD.Name = "lblWPD";
+            this.lblWPD.Size = new System.Drawing.Size(23, 25);
+            this.lblWPD.TabIndex = 3;
+            this.lblWPD.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Days Left: ";
+            // 
+            // lblRemainingDays
+            // 
+            this.lblRemainingDays.AutoSize = true;
+            this.lblRemainingDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblRemainingDays.Location = new System.Drawing.Point(134, 31);
+            this.lblRemainingDays.Name = "lblRemainingDays";
+            this.lblRemainingDays.Size = new System.Drawing.Size(23, 25);
+            this.lblRemainingDays.TabIndex = 5;
+            this.lblRemainingDays.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(298, 217);
+            this.ClientSize = new System.Drawing.Size(298, 434);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lblWCNum);
             this.Controls.Add(this.lblWC);
             this.Controls.Add(this.btnCheckOtherWordcount);
@@ -120,6 +225,9 @@
             this.Controls.Add(this.btnSetWordCount);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,6 +241,14 @@
         private System.Windows.Forms.Button btnCheckOtherWordcount;
         private System.Windows.Forms.Label lblWC;
         private System.Windows.Forms.Label lblWCNum;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblWordsRemaining;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblWPD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRemainingDays;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -81,6 +81,9 @@ namespace NaNoWriMo
             lblWCNum.Text = responseParsed["user_wordcount"].ToString();
             System.Drawing.Point point = new Point(this.Width / 2 - lblWCNum.Width / 2, lblWCNum.Location.Y);
             lblWCNum.Location = point;
+            lblWordsRemaining.Text = (50000 - int.Parse(responseParsed["user_wordcount"])).ToString();
+            lblRemainingDays.Text = (30 - DateTime.Now.Day).ToString();
+            lblWPD.Text = ((50000 - int.Parse(responseParsed["user_wordcount"])) / (30 - DateTime.Now.Day)).ToString(); ;
         }
     }
 }
